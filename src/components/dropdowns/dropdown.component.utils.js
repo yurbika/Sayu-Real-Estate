97af4abcf8150c8176aae678cf1e18ec8e92bf18
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-01-28 18:52:26 +0100

    fuer preis-max wird eine neue funktion geschrieben deswegen aus dem dropdown entfernt, createLi wurde in die dropdown utils verschoben

--- src/components/dropdowns/dropdown.component.utils.js
+++ src/components/dropdowns/dropdown.component.utils.js
@@ -1,3 +1,9 @@
+import React from "react";
+
+import { numberWithDots, removeDots } from "../input/input.utils";
+
+import { ID_GENERATOR } from "../../uniqueKey";
+
 import FilterActionTypes from "../../redux/filter/filter.types";
 
 //hier wird entschieden welcher setter benutz werden soll
@@ -26,4 +32,62 @@ export const typSetter = text => {
     default:
       return null;
   }
+};
+
+//dynamische werte fuer die liste der min preise
+export const createLi = (
+  n,
+  oppositeInputState,
+  oppositeInputName,
+  inputName,
+  setInput
+) => {
+  let i = 0;
+  let array = [];
+  let temp = 0;
+
+  //grenz werte fuer mieten
+  if (Number(removeDots(oppositeInputState)) < 2000) temp = 2000;
+  else if (Number(removeDots(oppositeInputState)) >= 10000) temp = 10000;
+  else temp = Number(removeDots(oppositeInputState));
+
+  //schritt weite der zahlen
+  while (i < n - 1) {
+    if (temp <= 3000) {
+      temp = Math.ceil(temp / 200) * 200;
+      temp -= 200;
+    } else if (temp > 3000 && temp <= 6000) {
+      temp = Math.ceil(temp / 500) * 500;
+      temp -= 500;
+    } else if (temp > 6000) {
+      temp = Math.ceil(temp / 1000) * 1000;
+      temp -= 1000;
+    }
+    array.push(
+      <li
+        onClick={e => {
+          setInput(e.currentTarget.textContent, inputName);
+          document.getElementById(oppositeInputName).click();
+          document.getElementById(oppositeInputName).focus();
+        }}
+        key={ID_GENERATOR("preis-dropdown-li-")}
+      >
+        {numberWithDots(temp.toString()) + "€"}
+      </li>
+    );
+    i++;
+  }
+  array.push(
+    <li
+      onClick={e => {
+        setInput(e.currentTarget.textContent, inputName);
+        document.getElementById(oppositeInputName).click();
+        document.getElementById(oppositeInputName).focus();
+      }}
+      key={ID_GENERATOR("preis-dropdown-li-")}
+    >
+      0€
+    </li>
+  );
+  return array.reverse();
 };
--- src/components/dropdowns/preis-dropdown.component.jsx
+++ src/components/dropdowns/preis-dropdown.component.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { ID_GENERATOR } from "../../uniqueKey";
+//utils
+import { createLi } from "./dropdown.component.utils";
 
 //component imports
 import Input from "../input/input.component";
-import {
-  numberWithDots,
-  onlyNumberkey,
-  removeDots
-} from "../input/input.utils";
+import { numberWithDots, onlyNumberkey } from "../input/input.utils";
 
 //redux imports
 import { dropdownRef } from "../../redux/dropdown/dropdown.utils";
@@ -31,59 +28,6 @@ import "./dropdowns.styles.scss";
 //wenn der dropdown geschlossen wird muss der state gesettz werden
 
 const PreisDropdown = ({ maxInput, minInput, setInput, bezugsart }) => {
-  const createLi = (n, oppositeInputState, oppositeInputName, inputName) => {
-    let i = 0;
-    let array = [];
-    let temp = 0;
-    let reverse = 1;
-    //grenz werte fuer mieten
-    if (inputName.toLowerCase() === "numbermax") reverse = -1;
-    if (Number(removeDots(oppositeInputState)) < 2000 && reverse) temp = 2000;
-    else if (Number(removeDots(oppositeInputState)) < 2000 && reverse === -1)
-      temp = 1800;
-    else if (Number(removeDots(oppositeInputState)) >= 10000) temp = 10000;
-    else temp = Number(removeDots(oppositeInputState));
-
-    while (i < n - 1) {
-      if (temp <= 3000) {
-        temp = Math.ceil(temp / 200) * 200;
-        temp -= 200 * reverse;
-      } else if (temp > 3000 && temp <= 6000) {
-        temp = Math.ceil(temp / 500) * 500;
-        temp -= 500 * reverse;
-      } else if (temp > 6000) {
-        temp = Math.ceil(temp / 1000) * 1000;
-        temp -= 1000 * reverse;
-      }
-      array.push(
-        <li
-          onClick={e => {
-            setInput(e.currentTarget.textContent, inputName);
-            document.getElementById(oppositeInputName).click();
-            document.getElementById(oppositeInputName).focus();
-          }}
-          key={ID_GENERATOR("preis-dropdown-li-")}
-        >
-          {numberWithDots(temp.toString()) + "€"}
-        </li>
-      );
-      i++;
-    }
-    array.push(
-      <li
-        onClick={e => {
-          setInput(e.currentTarget.textContent, inputName);
-          document.getElementById(oppositeInputName).click();
-          document.getElementById(oppositeInputName).focus();
-        }}
-        key={ID_GENERATOR("preis-dropdown-li-")}
-      >
-        {reverse === 1 ? "0€" : "Egal"}
-      </li>
-    );
-    return reverse === 1 ? array.reverse() : array;
-  };
-
   return (
     <div className="dropdown-container preis-dropdown" ref={dropdownRef}>
       <div className="input-container">
@@ -102,7 +46,7 @@ const PreisDropdown = ({ maxInput, minInput, setInput, bezugsart }) => {
           onKeyPress={e => onlyNumberkey(e)}
         />
         <ul id="preis-min">
-          {createLi(10, maxInput, "max-input", "numberMin")}
+          {createLi(10, maxInput, "max-input", "numberMin", setInput)}
         </ul>
       </div>
       <div>
@@ -122,9 +66,7 @@ const PreisDropdown = ({ maxInput, minInput, setInput, bezugsart }) => {
           onChange={e => setInput(numberWithDots(e.target.value), "numberMax")}
           onKeyPress={e => onlyNumberkey(e)}
         />
-        <ul id="preis-max">
-          {createLi(10, minInput, "min-input", "numberMax")}
-        </ul>
+        <ul id="preis-max">{/*funktion*/}</ul>
       </div>
     </div>
   );
