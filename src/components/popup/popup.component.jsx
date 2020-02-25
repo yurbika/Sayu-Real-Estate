56df44c19bed65ced525f68b24dcde5befe411d6
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-02-25 17:13:48 +0100

    popup component styling wurde die seitliche info hinzugefügt

--- src/components/popup/popup.component.jsx
+++ src/components/popup/popup.component.jsx
@@ -26,26 +26,39 @@ class Popup extends React.Component {
     return (
       <div className="popup-container">
         <div className="popup" ref={popupRef}>
-          <div className="popup-bild-preview-container">
-            <Slider
-              imgArray={[
-                immo[haustyp]["bilder"]["titelbild"] +
-                  "&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=350&fit=crop",
-                immo[haustyp]["bilder"]["zweites"] +
-                  "&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=350&fit=crop",
-                immo[haustyp]["bilder"]["drittes"] +
-                  "&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=350&fit=crop",
-                immo[haustyp]["bilder"]["vier"] +
-                  "&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=350&fit=crop",
-                immo[haustyp]["bilder"]["fünf"] +
-                  "&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=350&fit=crop",
-                immo[haustyp]["bilder"]["sechs"] +
-                  "&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=350&fit=crop"
-              ]}
-              alt={haustyp}
-              id={immoID}
-              additionalStyle={"popup-slider"}
-            />
+          <div className="side-info">
+            <div className="first">
+              <span>KURZINFORMATION</span>
+            </div>
+            <div className="second">
+              <span>VORSCHAU</span>
+            </div>
+          </div>
+          <div className="main-section">
+            <div className="kurzinfo">
+              <div className="popup-bild-preview-container">
+                <Slider
+                  imgArray={[
+                    immo[haustyp]["bilder"]["titelbild"] +
+                      "&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=350&fit=crop",
+                    immo[haustyp]["bilder"]["zweites"] +
+                      "&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=350&fit=crop",
+                    immo[haustyp]["bilder"]["drittes"] +
+                      "&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=350&fit=crop",
+                    immo[haustyp]["bilder"]["vier"] +
+                      "&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=350&fit=crop",
+                    immo[haustyp]["bilder"]["fünf"] +
+                      "&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=350&fit=crop",
+                    immo[haustyp]["bilder"]["sechs"] +
+                      "&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=350&fit=crop"
+                  ]}
+                  alt={haustyp}
+                  id={immoID}
+                  additionalStyle={"popup-slider"}
+                />
+              </div>
+              <div className="infos"></div>
+            </div>
           </div>
         </div>
       </div>
