5140ba116d38f41c62d65a770a02ae448b350fdd
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-03-13 10:20:58 +0100

    popup styled component vorbereitung

---
+++ src/components/popup/popup.styles.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import theme from "../../utils/theme";
+
+//allgemeine styles für den popup
+
+export const PopupContainer = styled.div``;
+
+export const Popup = styled.div``;
+
+export const CloseButtonContainer = styled.div``;
+
+export const AllContentContainer = styled.div``;
+
+export const RotateContainer = styled.div``;
+
+export const Rotate = styled.div``;
+
+export const Section = styled.div``;
+
+//sektions bedingte styles
+//kurzinfo
+export const Container =styled.div``
+
+//bild-info
+export const SliderInfoContainer = styled.div``
+
+export const SliderContainer = styled.div``
+
+export const SliderPreviewContainer = styled.div``
+
+export const InfosContainer = styled.div``
+
+export const IconContainer = styled.div``
+
+export const InfosContent = styled.div``
+
+//sideinfos
+export const BoxInfo = styled.div``
+
+export const InfosFooter = styled.div``
+
+const getAdditionalStylesSpan = props => {};
+
+export const HeaderSpan = styled.span``
