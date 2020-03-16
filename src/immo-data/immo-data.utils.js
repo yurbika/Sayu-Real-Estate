c9b7dde71372b7ab1759c6506cbc8a40d43121f1
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-03-16 21:39:45 +0100

    immo.data.utils converted

--- src/immo-data/immo-data.utils.js
+++ src/immo-data/immo-data.utils.js
@@ -14,33 +14,33 @@ const createRegex = splitedStr => {
 };
 
 export const filterData = (filter, data = IMMO_DATA) => {
-  let bundeslaenderArray = [];
-  let staedteOrteArray = [];
-  let straßenPlzOrtArray = [];
-  let alleErgebnisse = [];
-  let immoArray = [];
+  let federalStatesArray = [];
+  let citiesLocalitiesArray = [];
+  let streetsPostcodeLocalitiesArray = [];
+  let allResults = [];
+  let realEstateArray = [];
   let search = filter["search"];
-  let haustyp = filter["haustyp"].toLowerCase();
+  let realEstateType = filter["realEstateType"].toLowerCase();
   let minInput = !!filter["minInput"]
     ? Number(removeDots(filter["minInput"].toString()))
     : null;
   let maxInput = !!filter["maxInput"]
     ? Number(removeDots(filter["maxInput"].toString()))
     : null;
-  let bezugsart = filter["bezugsart"].toLowerCase();
-  let wohnfläche = !!filter["wohnfläche"]
-    ? Number(removeDots(filter["wohnfläche"].toString()))
+  let obtainingType = filter["obtainingType"].toLowerCase();
+  let livingspace = !!filter["livingspace"]
+    ? Number(removeDots(filter["livingspace"].toString()))
     : null;
-  let grundstück = !!filter["grundstück"]
-    ? Number(removeDots(filter["grundstück"].toString()))
+  let property = !!filter["property"]
+    ? Number(removeDots(filter["property"].toString()))
     : null;
-  let zimmer = !!filter["zimmerAnzahl"]
-    ? Number(removeDots(filter["zimmerAnzahl"].toString()))
+  let rooms = !!filter["rooms"]
+    ? Number(removeDots(filter["rooms"].toString()))
     : null;
-  let badezimmer = !!filter["badezimmer"]
-    ? Number(removeDots(filter["badezimmer"].toString()))
+  let bathrooms = !!filter["bathrooms"]
+    ? Number(removeDots(filter["bathrooms"].toString()))
     : null;
-  let suchtreffer = 0;
+  let hits = 0;
   let totalArrayLength = 0;
   let splitedStr = !!search ? search.split(/[ ,-]+/) : "";
   //entfernt alles leere
@@ -49,115 +49,118 @@ export const filterData = (filter, data = IMMO_DATA) => {
   //hier wird für bundesländer/ortschaften gefiltert davon ist die gesamt anzahl des arrays abhängig
   //deswegen gibt es dafür zwei for schleifen
   for (let i in data) {
-    if (!!!data[i][haustyp]) continue;
-    if (data[i][haustyp]["bezugsart"] !== bezugsart) continue;
+    if (!!!data[i][realEstateType]) continue;
+    if (data[i][realEstateType]["bezugsart"] !== obtainingType) continue;
     if (
-      !bundeslaenderArray.includes(data[i][haustyp]["adresse"]["bundesland"]) &&
-      !!data[i][haustyp]["adresse"]["bundesland"].match(regex) &&
-      bundeslaenderArray.length < 4 &&
+      !federalStatesArray.includes(
+        data[i][realEstateType]["adresse"]["bundesland"]
+      ) &&
+      !!data[i][realEstateType]["adresse"]["bundesland"].match(regex) &&
+      federalStatesArray.length < 4 &&
       splitedStr.length > 0
     ) {
-      bundeslaenderArray.push(data[i][haustyp]["adresse"]["bundesland"]);
+      federalStatesArray.push(data[i][realEstateType]["adresse"]["bundesland"]);
     }
     if (
-      !staedteOrteArray.includes(
-        data[i][haustyp]["adresse"]["stadt"] +
+      !citiesLocalitiesArray.includes(
+        data[i][realEstateType]["adresse"]["stadt"] +
           " - " +
-          data[i][haustyp]["adresse"]["bundesland"]
+          data[i][realEstateType]["adresse"]["bundesland"]
       ) &&
-      !!data[i][haustyp]["adresse"]["stadt"].match(regex) &&
-      staedteOrteArray.length < 4 &&
+      !!data[i][realEstateType]["adresse"]["stadt"].match(regex) &&
+      citiesLocalitiesArray.length < 4 &&
       splitedStr.length > 0
     ) {
-      staedteOrteArray.push(
-        data[i][haustyp]["adresse"]["stadt"] +
+      citiesLocalitiesArray.push(
+        data[i][realEstateType]["adresse"]["stadt"] +
           " - " +
-          data[i][haustyp]["adresse"]["bundesland"]
+          data[i][realEstateType]["adresse"]["bundesland"]
       );
     }
   }
-  totalArrayLength = 12 - (bundeslaenderArray.length + staedteOrteArray.length);
+  totalArrayLength =
+    12 - (federalStatesArray.length + citiesLocalitiesArray.length);
   for (let i in data) {
-    if (!!!data[i][haustyp]) continue;
-    if (data[i][haustyp]["bezugsart"] !== bezugsart) continue;
+    if (!!!data[i][realEstateType]) continue;
+    if (data[i][realEstateType]["bezugsart"] !== obtainingType) continue;
     if (
-      !straßenPlzOrtArray.includes(
-        data[i][haustyp]["adresse"]["straße"] +
+      !streetsPostcodeLocalitiesArray.includes(
+        data[i][realEstateType]["adresse"]["straße"] +
           ", " +
-          data[i][haustyp]["adresse"]["postleitzahl"] +
+          data[i][realEstateType]["adresse"]["postleitzahl"] +
           " - " +
-          data[i][haustyp]["adresse"]["stadt"] +
+          data[i][realEstateType]["adresse"]["stadt"] +
           " - " +
-          data[i][haustyp]["adresse"]["bundesland"]
+          data[i][realEstateType]["adresse"]["bundesland"]
       ) &&
       (
-        data[i][haustyp]["adresse"]["straße"] +
+        data[i][realEstateType]["adresse"]["straße"] +
         ", " +
-        data[i][haustyp]["adresse"]["postleitzahl"] +
+        data[i][realEstateType]["adresse"]["postleitzahl"] +
         " - " +
-        data[i][haustyp]["adresse"]["stadt"] +
+        data[i][realEstateType]["adresse"]["stadt"] +
         " - " +
-        data[i][haustyp]["adresse"]["bundesland"]
+        data[i][realEstateType]["adresse"]["bundesland"]
       ).match(regex)
     ) {
       if (minInput !== 0 && !!minInput) {
-        if (data[i][haustyp]["preis"] < minInput) continue;
+        if (data[i][realEstateType]["preis"] < minInput) continue;
       }
       if (maxInput !== 0 && !!maxInput) {
-        if (data[i][haustyp]["preis"] > maxInput) continue;
+        if (data[i][realEstateType]["preis"] > maxInput) continue;
       }
-      if (!!wohnfläche) {
-        if (data[i][haustyp]["wohnfläche"] < wohnfläche) continue;
+      if (!!livingspace) {
+        if (data[i][realEstateType]["wohnfläche"] < livingspace) continue;
       }
-      if (!!grundstück) {
-        if (data[i][haustyp]["grundstück"] < grundstück) continue;
+      if (!!property) {
+        if (data[i][realEstateType]["grundstück"] < property) continue;
       }
-      if (!!zimmer) {
-        if (data[i][haustyp]["zimmer"] < zimmer) continue;
+      if (!!rooms) {
+        if (data[i][realEstateType]["zimmer"] < rooms) continue;
       }
-      if (!!badezimmer) {
-        if (data[i][haustyp]["zimmer"] < badezimmer) continue;
+      if (!!bathrooms) {
+        if (data[i][realEstateType]["badezimmer"] < bathrooms) continue;
       }
       if (
-        straßenPlzOrtArray.length < totalArrayLength &&
+        streetsPostcodeLocalitiesArray.length < totalArrayLength &&
         splitedStr.length > 0
       ) {
-        straßenPlzOrtArray.push(
-          data[i][haustyp]["adresse"]["straße"] +
+        streetsPostcodeLocalitiesArray.push(
+          data[i][realEstateType]["adresse"]["straße"] +
             ", " +
-            data[i][haustyp]["adresse"]["postleitzahl"] +
+            data[i][realEstateType]["adresse"]["postleitzahl"] +
             " - " +
-            data[i][haustyp]["adresse"]["stadt"] +
+            data[i][realEstateType]["adresse"]["stadt"] +
             " - " +
-            data[i][haustyp]["adresse"]["bundesland"]
+            data[i][realEstateType]["adresse"]["bundesland"]
         );
-        alleErgebnisse.push(data[i]);
+        allResults.push(data[i]);
       } else if (!!!search) {
-        immoArray.push(data[i]);
-      } else if (splitedStr.length > 0) alleErgebnisse.push(data[i]);
-      suchtreffer++;
+        realEstateArray.push(data[i]);
+      } else if (splitedStr.length > 0) allResults.push(data[i]);
+      hits++;
     }
   }
-  /*zufällige ergebnisse für immoArray welches für die
+  /*zufällige ergebnisse für realEstateArray welches für die
   Inspirationssektion ist*/
-  while (immoArray.length > 12) {
-    let randomNum = Math.floor(Math.random() * immoArray.length);
-    immoArray.splice(randomNum, 1);
+  while (realEstateArray.length > 12) {
+    let randomNum = Math.floor(Math.random() * realEstateArray.length);
+    realEstateArray.splice(randomNum, 1);
   }
-  //wenn das erste zeichen, ein leerzeichen ist wird suchtreffer zurückgesetzt
+  //wenn das erste zeichen, ein leerzeichen ist wird hits zurückgesetzt
   if (
-    straßenPlzOrtArray.length === 0 &&
-    bundeslaenderArray.length === 0 &&
-    staedteOrteArray.length === 0
+    streetsPostcodeLocalitiesArray.length === 0 &&
+    federalStatesArray.length === 0 &&
+    citiesLocalitiesArray.length === 0
   ) {
-    suchtreffer = 0;
+    hits = 0;
   }
   return {
-    bundeslaenderArray,
-    staedteOrteArray,
-    straßenPlzOrtArray,
-    suchtreffer,
-    immoArray,
-    alleErgebnisse
+    federalStatesArray,
+    citiesLocalitiesArray,
+    streetsPostcodeLocalitiesArray,
+    hits,
+    realEstateArray,
+    allResults
   };
 };
